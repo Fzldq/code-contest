7ebc9116afe093bc4853e263875885f6d68a4478
zcr1992 <qq417856426@gmail.com>
2020-04-19 17:28:23 +0900

    debug

--- read.py
+++ read.py
@@ -1,4 +1,13 @@
+import heapq
+from itertools import combinations
+from itertools import permutations
+from collections import defaultdict
+from collections import Counter
+from collections import deque
+
+
 import sys
 read = sys.stdin.read
 readline = sys.stdin.readline
 readlines = sys.stdin.readlines
+sys.setrecursionlimit(1000000)
